a4cc6b5f7a9785034edff4153d91a8c38f7167ce
Andreas Wunder <a.wunder@liv-t.com>
2021-01-08 18:43:51 +0100

    add no file handling

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from streamlit_pandas_profiling import st_profile_report
 st.title('csv profiler')
 
 csv_file = st.file_uploader(label='CSV file to profile', type='csv')
-data = pd.read_csv(csv_file, error_bad_lines=False)
-pr = ProfileReport(data, explorative=True)
+if csv_file is not None:
+    data = pd.read_csv(csv_file, error_bad_lines=False)
+    pr = ProfileReport(data, explorative=True)
 
-st_profile_report(pr)
+    st_profile_report(pr)
